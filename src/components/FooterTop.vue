87a481ecfb7d33af85bb46185982d957e6a8bf94
EliaMagris <elia.magris@libero.it>
2022-12-03 15:57:12 +0100

    inizio footer

--- src/components/FooterTop.vue
+++ src/components/FooterTop.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="hello">
+  <div>
+    <div class="techCommunity">
+      <div class="forumCont">
+        <h1>Hei! Joiun to our commuity</h1>
+        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate consequuntur ducimus accusantium ipsum! Laborum ratione qui assumenda aperiam obcaecati ea voluptate pariatur!</div>
+        <button>GET AVADA NOW!<font-awesome-icon class="fa-solid" icon="fa-solid fa-chevron-right" /></button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -7,12 +14,36 @@
 export default {
   name: 'FooterTop',
   props: {
-  
+    
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.techCommunity{
+  background-image: url(../assets/join_now_cta_bg.jpg);
 
+  .forumCont{
+    text-align: center;
+    width: 40%;
+    margin: auto;
+    padding: 200px 0;
+    color: white;
+    margin-bottom: 50px;
+    h1{
+      font-size: 4rem;
+    }
+    div{
+      padding: 35px 0;
+    }
+     button {
+        background-color: #27aabe;
+        border: none;
+        padding: 10px 15px;
+        font-weight: bold;
+        color: white;
+     }
+  }
+}
 </style>
